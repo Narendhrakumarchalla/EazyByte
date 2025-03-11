ceb565cdd6fb1a27345f78ccf3d1695328b00e0a
Narendhra kumar challa <naren@gmail.com>
2025-03-11 11:51:04 +0530

    all changed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (
-    <div className='text-red-500'> App</div>
+    <Routes>
+      <Route path='/' element={<Navbar/>}/>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+    </Routes>
   )
 }
-
 export default App
